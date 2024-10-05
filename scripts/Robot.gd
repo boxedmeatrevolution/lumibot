cddc65997b8c513ef3cc62c732972f8e82c8454a
Duane Byer <duane.byer@gmail.com>
2024-10-05 12:22:37 -0700

    Add basic robot animation

--- scripts/Robot.gd
+++ scripts/Robot.gd
@@ -1,11 +1,27 @@
 extends Node3D
 
 var velocity : Vector3 = Vector3(0, 0, 0)
+@onready var animation_player := $AnimationPlayer
+
+enum State {
+	STAND,
+	WALK
+}
+
+var state : State = State.STAND
 
 func _ready() -> void:
-	pass
+	animation_player.play("STAND")
 
 func _process(delta: float) -> void:
-	if randf() > exp(-delta / 3):
-		velocity = Vector3(randf_range(-2, 2), 0, randf_range(-1, 1))
-	position += velocity * delta
+	if state == State.STAND:
+		if randf() > exp(-delta / 3):
+			state = State.WALK
+			animation_player.play("WALK", 0.5)
+			velocity = Vector3(randf_range(-2, 2), 0, randf_range(-1, 1))
+	elif state == State.WALK:
+		position += velocity * delta
+		if randf() > exp(-delta / 3):
+			state = State.STAND
+			animation_player.play("STAND", 0.5)
+			velocity = Vector3(0, 0, 0)
