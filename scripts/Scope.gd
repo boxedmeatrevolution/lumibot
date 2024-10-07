74814bfa288d7853121f718495819297ab34c0d5
Duane Byer <duane.byer@gmail.com>
2024-10-07 15:18:51 -0700

    Escape to quit

--- scripts/Scope.gd
+++ scripts/Scope.gd
@@ -61,10 +61,14 @@ func _ready() -> void:
 	
 	var keyShift = InputEventKey.new()
 	keyShift.physical_keycode = KEY_SHIFT
+	var keyEescape = InputEventKey.new()
+	keyEescape.physical_keycode = KEY_ESCAPE
 	
 	InputMap.add_action("shoot")
 	InputMap.action_add_event("shoot", mouseButtonLeft)
 	InputMap.action_add_event("shoot", keyShift)
+	InputMap.add_action("exit")
+	InputMap.action_add_event("exit", keyEescape)
 	
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
@@ -80,6 +84,9 @@ func _ready() -> void:
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if Input.is_action_pressed("exit"):
+		print("QUIT")
+		get_tree().quit()
 	if !dead && Input.is_action_pressed("zoom"):
 		zoom_in()
 	else:
