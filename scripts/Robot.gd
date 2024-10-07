c8f38b9db861175adb1705c05ab3ffa0e2de70c6
Duane Byer <duane.byer@gmail.com>
2024-10-06 19:56:47 -0700

    Missile animation

--- scripts/Robot.gd
+++ scripts/Robot.gd
@@ -5,6 +5,7 @@ const Building := preload("res://scripts/Building.gd")
 
 @onready var animation_player := $AnimationPlayer
 @onready var grab_point := $GrabPoint
+@onready var fire_point := $FirePoint
 @onready var player : Node3D = owner.find_child("Camera3D")
 
 var velocity := Vector3.ZERO
@@ -16,7 +17,8 @@ const STATE_MAX_TIME : float = 5
 enum State {
 	STAND,
 	WALK,
-	THROW
+	THROW,
+	BARRAGE
 }
 
 var state : State = State.STAND
@@ -31,16 +33,15 @@ func _process(delta: float) -> void:
 		state_timer = STATE_MAX_TIME + 1
 	var at_min_time := (state_timer > STATE_MIN_TIME)
 	var at_max_time := (state_timer > STATE_MAX_TIME)
-	if randf() > exp(-delta / 3):
-		var rocket = RocketScene.instantiate()
-		get_parent().add_child(rocket)
-		rocket.global_position = global_position
-		rocket.target = player.global_position
 	if state == State.STAND:
 		if building != null && at_min_time && (at_max_time || randf() > exp(-delta / 3)):
 			state = State.THROW
 			state_timer = 0
 			animation_player.play("THROW", 0.5, 0.4)
+		if building == null && at_min_time && (at_max_time || randf() > exp(-delta / 3)):
+			state = State.BARRAGE
+			state_timer = 0
+			animation_player.play("BARRAGE", 0.5, 0.5)
 		if at_min_time && (at_max_time || randf() > exp(-delta / 3)):
 			state = State.WALK
 			state_timer = 0
@@ -53,7 +54,7 @@ func _process(delta: float) -> void:
 			state_timer = 0
 			animation_player.play("STAND", 0.5, 0.5)
 			velocity = Vector3.ZERO
-	elif state == State.THROW:
+	elif state == State.THROW || state == State.BARRAGE:
 		if !animation_player.is_playing():
 			state = State.STAND
 			state_timer = 0
@@ -64,6 +65,12 @@ func throw_building():
 	if building != null:
 		building.throw(get_parent(), player.global_position + 1 * Vector3.DOWN)
 		building = null
+
+func fire_missile():
+		var rocket = RocketScene.instantiate()
+		get_parent().add_child(rocket)
+		rocket.global_position = fire_point.global_position
+		rocket.target = player.global_position
 
 func _on_area_entered(area: Area3D) -> void:
 	if area.get_collision_layer_value(4):
